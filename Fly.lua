ff52408f49037e64a995b19402686803c22b42f0
moxuvn-boop <moxuvn@gmail.com>
2025-12-24 01:53:58 +0700

    Create Fly.lua

---
+++ Fly.lua
@@ -0,0 +1,8 @@
+--// Fly
+--// Adjust the keybinds to your liking.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/moxuvn-boop/Fly/refs/heads/main/main.lua", true))()
+Fly.Keybinds.Fly = "F" or "ButtonA"
+Fly.Keybinds.Hide = "H" or "ButtonB"
+Fly.Keybinds.Remove = "R" or "ButtonC" 
+Fly.NoGravity = true
+Fly.FaceCamera = true
